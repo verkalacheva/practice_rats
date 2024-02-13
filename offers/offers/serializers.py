ef91a82d2ee39275ec35572fe52b0197e5f158f2
MarkovDV17 <113051806+MarkovDV17@users.noreply.github.com>
2024-02-13 23:48:56 +0300

    Delete offers directory

--- offers/offers/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models  import*
-
-class OfferSerializer(serializers.ModelSerializer):
-    class Meta():
-        model = Offer
-        fields = "__all__"
--- offers/offers/urls.py
+++
@@ -1,2 +0,0 @@
-from .views import *
-from django.urls import path, include
