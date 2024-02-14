dda8f8c6f7d51a8524712b649e0e8a0d6a217f7e
verkalacheva <verkalacheva@mail.ru>
2024-02-15 00:48:40 +0300

    fix model

--- events/rat/urls.py
+++ events/rat/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 from django.conf.urls.static import static
-from rest_framework_swagger.views import get_swagger_view
+#from rest_framework_swagger.views import get_swagger_view
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
